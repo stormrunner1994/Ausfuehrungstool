f5a2af066492820cb4d5c22453b4bb105d0a9c0f
Lars Pfennig <lars.pfennig@googlemail.com>
2022-04-04 20:57:18 +0200

    Projektdateien hinzufügen.

---
+++ Ausführungstool/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Ausführungstool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxausgabe = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBoxeingabe = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBoxausgabe
+            // 
+            this.richTextBoxausgabe.Location = new System.Drawing.Point(6, 19);
+            this.richTextBoxausgabe.Name = "richTextBoxausgabe";
+            this.richTextBoxausgabe.Size = new System.Drawing.Size(181, 156);
+            this.richTextBoxausgabe.TabIndex = 0;
+            this.richTextBoxausgabe.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBoxeingabe
+            // 
+            this.richTextBoxeingabe.Location = new System.Drawing.Point(20, 19);
+            this.richTextBoxeingabe.Name = "richTextBoxeingabe";
+            this.richTextBoxeingabe.Size = new System.Drawing.Size(155, 156);
+            this.richTextBoxeingabe.TabIndex = 2;
+            this.richTextBoxeingabe.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(461, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Aufzeichnen starten";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(116, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Abspielen";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(461, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Lese Log";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBoxeingabe);
+            this.groupBox1.Location = new System.Drawing.Point(27, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(193, 186);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Eingabe:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBoxausgabe);
+            this.groupBox2.Location = new System.Drawing.Point(245, 122);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(193, 186);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ausgabe:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(333, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Clipboard:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(251, 314);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Set Aufzeichnung";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxausgabe;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBoxeingabe;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Ausführungstool/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hardware_;
+
+namespace Ausführungstool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            hw = new Hardware(richTextBoxausgabe, button1, 100000, this, null, richTextBoxeingabe, false);
+        }
+
+        Hardware hw;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hw.ReagiereAufKey();
+          }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Aufzeichnen starten")
+            {
+                hw.AufzeichnungStarten(InputModus.KeyUndMaus);
+                button2.Text = "Aufzeichnen stoppen";            
+                }
+            else
+            {
+                hw.AufzeichnungStopp(InputModus.KeyUndMaus);
+                button2.Text = "Aufzeichnen starten";
+                hw.showAufzeichnung(richTextBoxausgabe);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "Abspielen")
+            {
+                hw.Starten(textBox1.Text);
+                button3.Text = "Stoppen";
+            }
+            else
+            {
+                hw.Stoppen();
+                button3.Text = "Abspielen";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            hw.loadAufzeichnung("log.txt");
+            hw.showAufzeichnung(richTextBoxausgabe);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            hw.SetAufzeichnung(richTextBoxausgabe.Text.Split('\n').ToList());
+        }
+    }
+}
